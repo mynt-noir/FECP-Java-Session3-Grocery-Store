ebcbc8648bf7757cbe015fdb78f211a794d23ef0
mynt-noir <nuanpatricia.serrano@gcash.com>
2025-07-07 16:33:06 +0800

    Final Submission, Lab 1: Part 1 of 2 Grocery Store Inventory System

--- src/test/java/org/example/InventoryTest.java
+++ src/test/java/org/example/InventoryTest.java
@@ -1,7 +1,97 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class InventoryTest {
+
+    private Inventory inventory;
+    private HashMap<String, Integer> products;
+
+
+    @BeforeEach
+    public void setUp() {
+        HashMap<String, Integer> products = new HashMap<>();
+        inventory = new Inventory(products);
+        inventory.addProduct(products, "Milk", 100);
+    }
+
+    @Test
+    void addProductValidQuantity() {
+        HashMap<String, Integer> products = new HashMap<>();
+        inventory = new Inventory(products);
+        boolean addProductSuccess = inventory.addProduct(products, "Bread", 50);
+
+        assertTrue(addProductSuccess);
+    }
+//
+//
+//    @Test
+//    void addProductInvalidQuantity {
+//
+//    }
+//
+//
+//    @Test
+//    void addProductZeroQuantity {
+//
+//    }
+//
+//
+//    @Test
+//    void checkProductValidName {
+//
+//    }
+//
+//
+//    @Test
+//    void checkProductInvalidName {
+//
+//    }
+//
+//
+//    @Test
+//    void updateStockValidName {
+//
+//    }
+//
+//
+//    @Test
+//    void updateStockInvalidName {
+//
+//    }
+//
+//    @Test
+//    void updateStockValidQuantity {
+//
+//    }
+//
+//    @Test
+//    void updateStockInvalidQuantity {
+//
+//    }
+//
+//    @Test
+//    void updateStockZeroQuantity {
+//
+//    }
+//
+//    @Test
+//    void removeProductValidName {
+//
+//    }
+//
+//
+//    @Test
+//    void removeProductInvalidName {
+//
+//    }
+//
 
 }
